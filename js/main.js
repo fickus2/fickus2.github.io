6b1ebd6d3a3f7a45c22af7e48b63927e4d334d64
Catink123 <qwerty.qaz120@gmail.com>
2019-10-28 22:01:55 +0300

    xx

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function link(path) {
+    $(".container").fadeOut(300, function () {
+        // $(".loading").fadeIn(150);
+        $(".container").load(path, function () {
+            // $(".loading").fadeOut(150);
+            $(".container").fadeIn(150);
+            p = path.replace("index.html", "");
+            history.pushState(p, p, p);
+        });
+    });
+}
