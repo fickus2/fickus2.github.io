e5165533483c52faa71a671f036a3218db195329
Catink123 <qwerty.qaz120@gmail.com>
2019-10-29 10:26:29 +0300

    Xx

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 function link(path) {
-    $(".container").fadeOut(300, function () {
+    $(".blurb").fadeOut(300, function () {
         // $(".loading").fadeIn(150);
-        $(".container").load(path + " #blurb", function () {
+        $(".blurb").load(path + " #blurb", function () {
             // $(".loading").fadeOut(150);
-            $(".container").fadeIn(150);
+            $(".blurb").fadeIn(150);
             p = path.replace("index.html", "");
             history.pushState(p, p, p);
         });
