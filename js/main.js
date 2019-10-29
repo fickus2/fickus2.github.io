5361a805440e506e3dd319cb605eaa88740d6ba0
Catink123 <qwerty.qaz120@gmail.com>
2019-10-29 10:15:54 +0300

    Xx

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function link(path) {
     $(".container").fadeOut(300, function () {
         // $(".loading").fadeIn(150);
-        $(".container").load(path + ".blurb", function () {
+        $(".container").load(path + " #blurb", function () {
             // $(".loading").fadeOut(150);
             $(".container").fadeIn(150);
             p = path.replace("index.html", "");
