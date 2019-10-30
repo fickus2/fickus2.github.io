766d5c5397a27023bac524c2137b5736675eb849
Catink123 <qwerty.qaz120@gmail.com>
2019-10-30 18:49:26 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 function link(path) {
     $(".container").fadeOut(150, function () {
         // $(".loading").fadeIn(150);
-        $(".container").load(path + " .blurb", function () {
-            // $(".loading").fadeOut(150);
+        $.get(path, data => {
+            $(".container").html($(".blurb", data))
             $(".container").fadeIn(150);
             p = path.replace("index.html", "");
             history.pushState(p, p, p);
-        });
+        })
+//         $(".container").load(path + " .blurb", function () {
+//             // $(".loading").fadeOut(150);
+//             $(".container").fadeIn(150);
+//             p = path.replace("index.html", "");
+//             history.pushState(p, p, p);
+//         });
     });
 }
 
-function createParallax(image) {
+function createParallax(place, image) {
     var par = document.createElement("div");
     par.className = "parallax";
     $(par).css("background", `url(${image})`);
-    $(".blurb").append(par);
+    $(place).append(par);
 }
